05dcb9690f06562fb53cbb116345b5044aff8628
son1112 <anderson.reinkordt@gmail.com>
2018-11-28 14:11:33 -0600

    cleanup, simplification, linting

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,32 +1,41 @@
-// Package space - given an age in seconds, calculate how old someone would be on
-// other planets
+// Package space calculates how old someone would be on a given planet by their
+// age in earth seconds
 package space
 
+// Planet is a type of string
 type Planet string
 
-// Age: Float, String => Integer
-// Takes age in seconds and planet name
-// Returns age in planet's years
+// Age converts earth seconds to given planet's years
 func Age(seconds float64, planet Planet) float64 {
 	return float64(seconds) / YearSeconds(planet)
 }
 
-// YearSeconds: String => Float
-// Takes planet name
-// Returns planet year in seconds
+// YearSeconds returns a given planet's year in seconds
 func YearSeconds(planet Planet) float64 {
 	var base float64 = 31557600
 
-	var planetYears = map[Planet]float64{
-		"Earth":   1,
-		"Mercury": 0.2408467,
-		"Venus":   0.61519726,
-		"Mars":    1.8808158,
-		"Jupiter": 11.862615,
-		"Saturn":  29.447498,
-		"Uranus":  84.016846,
-		"Neptune": 164.79132,
+	planetYears := func(planet Planet) float64 {
+		switch planet {
+		case "Earth":
+			return 1
+		case "Mercury":
+			return 0.2408467
+		case "Venus":
+			return 0.61519726
+		case "Mars":
+			return 1.8808158
+		case "Jupiter":
+			return 11.862615
+		case "Saturn":
+			return 29.447498
+		case "Uranus":
+			return 84.016846
+		case "Neptune":
+			return 164.79132
+		default:
+			return 1
+		}
 	}
 
-	return base * planetYears[planet]
+	return base * planetYears(planet)
 }
