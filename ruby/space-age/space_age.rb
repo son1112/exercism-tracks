7a419212658939702ff4f1e580688c1f53c24832
son1112 <anderson.reinkordt@gmail.com>
2018-12-07 12:11:32 -0600

    clarification and refactoring of constants; meta define class methods once instead of on every instance

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,29 +1,32 @@
-BASE = 315_576_00.0
-
-PLANETS = {
-  'earth' => 1.0,
-  'mercury' => 0.2408467,
-  'venus' => 0.61519726,
-  'mars' => 1.8808158,
-  'jupiter' => 11.862615,
-  'saturn' => 29.447498,
-  'uranus' => 84.016846,
-  'neptune' => 164.79132
-}.freeze
-
 # SpaceAge calculates how old someone would be on different planets from seconds
 class SpaceAge
-  def initialize(seconds)
-    @seconds = seconds
-    define_planet_methods
+  EARTH_ORBITAL = 315_576_00.0 # seconds
+
+  PLANETS = {
+    'earth' => 1.0,
+    'mercury' => 0.2408467,
+    'venus' => 0.61519726,
+    'mars' => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn' => 29.447498,
+    'uranus' => 84.016846,
+    'neptune' => 164.79132
+  }.freeze
+
+  PLANETS.each do |planet, earth_years|
+    method_name = "on_#{planet}".to_sym
+    define_method(method_name) do
+      calculate_age.call(earth_years)
+    end
   end
 
-  def define_planet_methods
-    PLANETS.each do |planet, earth_years|
-      method_name = "on_#{planet}".to_sym
-      self.class.define_method(method_name) do
-        @seconds / (BASE * earth_years)
-      end
-    end
+  attr_reader :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  def calculate_age
+    proc { |earth_years| seconds / (EARTH_ORBITAL * earth_years) }
   end
 end
