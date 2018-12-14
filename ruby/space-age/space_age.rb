6a002cea159e355d8fe61ccbe1fbbb69b05f8c12
son1112 <anderson.reinkordt@gmail.com>
2018-12-14 12:25:21 -0600

    minor

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -3,18 +3,18 @@ class SpaceAge
   EARTH_ORBITAL = 315_576_00.0 # seconds
 
   PLANETS = {
-    'earth' => 1.0,
-    'mercury' => 0.2408467,
-    'venus' => 0.61519726,
-    'mars' => 1.8808158,
-    'jupiter' => 11.862615,
-    'saturn' => 29.447498,
-    'uranus' => 84.016846,
-    'neptune' => 164.79132
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }.freeze
 
   PLANETS.each do |planet, earth_years|
-    method_name = "on_#{planet}".to_sym
+    method_name = "on_#{planet}"
     define_method(method_name) do
       calculate_age(earth_years)
     end
