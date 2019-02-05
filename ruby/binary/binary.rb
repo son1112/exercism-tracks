e2567d4ea60248f5cd8386ebf090caf04a5fa170
son1112 <anderson.reinkordt@gmail.com>
2019-02-05 07:28:46 -0600

    remove unnecessary collect, use sum

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,12 +1,12 @@
 # Binary converts a binary string to its decimal integer
 module Binary
-  def self.to_decimal(binary_string)
-    validate(binary_string)
+  def self.to_decimal(bin_str)
+    validate(bin_str)
 
-    binary_string.each_char.with_index.collect do |char, index|
-      power = (binary_string.length - 1) - index
+    bin_str.chars.each_with_index.sum do |char, index|
+      power = (bin_str.length - 1) - index
       binary_to_denary(char.to_i, power)
-    end.sum
+    end
   end
 
   def self.binary_to_denary(digit, power)
